1f8a04b21778c9c5963e7153499c29199ae41629
Pat Gunn <pgunn@dachte.org>
2011-07-19 01:04:23 -0400

    Small changes Kep suggested

--- importdaemon.py
+++ importdaemon.py
@@ -9,27 +9,32 @@ if sys.version < '2.7':
 	print "Requires Python 2.7, you have", sys.version
 	sys.exit(8)
 
+######################
+# TODO:
+#	Rework to avoid underscores, as per python convention
 
 #################################################
 # Source/Policy management
 
 def get_source_list():
 
-def get_eligible_sources():
-# An eligible source is one that we should retrieve now
+def get_stale_sources():
+# A stale source is one that we should retrieve now
 #	We define this as:
-#	LAST_RETRIEVED + RETRIEVAL_FREQUENCY < NOW
+#	LAST_RETRIEVED + UPDATE_INTERVAL < NOW
 #
 #	When we successfully check a source, and if it either:
 #	A) Does not have new data for us, or
 #	B) We integrate any new data
 #	We update LAST_RETRIEVED to NOW (CACHE that!)
 
-def define_source(source, sourcetype, site, path=None, user=None, pass=None):
+def define_source(source, sourcetype, site=None, path=None, user=None, pass=None):
 # 	"source" is a short name of a source
-#	sourcetype is one of (ftp, http, pop3, scp, file)
+#	sourcetype is one of (ftp, http, https, pop3, scp, file)
 #		Not all of these will initially be implemented
 #	site is a hostname of where to retrieve files. Blank for "file" sourcetype
+#	path is URL components after the hostname, or FTP components, or ...
+#		leave blank for pop3
 #	user is the login name for the resource (if applicable)
 #	pass is the password for the resource (if applicable)
 #
@@ -44,15 +49,23 @@ def get_file(source, filename):
 
 #################################################
 # Source identity functions
+# 	Do we already have this chunk of data?
 
-def get_source_info(source):
 
-def source_what_do_i_lack(source, cachedir):
+def getNeededSources(source, cachedir):
 # Is this better, or..
 def source_do_ihave(source, filename):
 # Is this better?
+
+#################################################
+# Misc
+
+def get_source_info(source):
+# Returns info from define_source
+
 def source_retrieve(source):
 # Needed for some sourcetypes, like pop3 and *maybe* file?
+#	For these sources, if there is data, it is imported.
 
 #################################################
 # Data retrieval functions
@@ -66,7 +79,7 @@ def get_httpfile(url):
 #################################################
 # Local cache management functions
 
-def build_cache_filename(source, origfn):
+def build_cache_filename(source, filename):
 # Use a predefined cachedir, sourcename, and sourcetype to attempt to integrate data into a source-specific
 # subdirectory so that:
 #	1) We can identify when we already have a certain file, for protocols (like FTP) where these might stick around
