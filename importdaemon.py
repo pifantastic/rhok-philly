aa689f9bf6ae72690cb390bb63652ba5322ef621
Pat Gunn <pgunn@dachte.org>
2011-07-25 00:31:31 -0400

    Implement more of the importdaemon functions

--- importdaemon.py
+++ importdaemon.py
@@ -3,8 +3,18 @@
 # Invoked from cron, reads config file, retrieves new data from various sources.
 # Saves files if they are files. Drops HTTP or POP3 data into files, otherwise.
 # Invokes importbackend.py appropriately.
+#
+#########################################
+# Design issues:
+#	1) Should this be configured primarily from a dotfile or from the
+#		database?
+#	2) Should this be invokable by the CGI or just by cron/manual invoke?
+#	3) Which account will this script run under?
+
 
 import os, sys, urllib, urllib2, getopt, csv, datetime, config, glob, subprocess
+import config	# If this fails, move the template to an actual file
+from ftplib import FTP
 
 if sys.version < '2.7':
 	print "Requires Python 2.7, you have", sys.version
@@ -18,28 +28,48 @@ if sys.version < '2.7':
 # Source/Policy management
 
 def get_source_list():
+	''' Returns datsourceid for each defined datasource
+	'''
+	dbconn = psycopg2.connect(PSQL_CONN_STRING)
+	curs = dbconn.cursor()
+	curs.execute("SELECT datsourceid FROM datasource;")
+	return curs.fetchall()
 
 def get_stale_sources():
-# A stale source is one that we should retrieve now
-#	We define this as:
-#	LAST_RETRIEVED + UPDATE_INTERVAL < NOW
-#
-#	When we successfully check a source, and if it either:
-#	A) Does not have new data for us, or
-#	B) We integrate any new data
-#	We update LAST_RETRIEVED to NOW (CACHE that!)
+	''' A stale source is one that we should retrieve now
+	We define this as:
+	LAST_RETRIEVED + UPDATE_INTERVAL < NOW
 
-def define_source(source, sourcetype, site=None, path=None, user=None, pass=None):
-# 	"source" is a short name of a source
-#	sourcetype is one of (ftp, http, https, pop3, scp, file)
-#		Not all of these will initially be implemented
-#	site is a hostname of where to retrieve files. Blank for "file" sourcetype
-#	path is URL components after the hostname, or FTP components, or ...
-#		leave blank for pop3
-#	user is the login name for the resource (if applicable)
-#	pass is the password for the resource (if applicable)
-#
-#	We may need to figure out a way to work geo and time information in here? Maybe?
+	When we successfully check a source, and if it either:
+	A) Does not have new data for us, or
+	B) We integrate any new data
+	We update LAST_RETRIEVED to NOW (CACHE that!) '''
+	dbconn = psycopg2.connect(PSQL_CONN_STRING)
+	stalesources = []
+	for i in get_source_list():
+		curs = dbconn.cursor()
+		curs.execute("SELECT source_is_current(%s)", (i,))
+		res = curs.fetchone()[0]
+		if(res):
+			stalesources.append(i)
+	return stalesources
+	
+
+def define_source(sourcename, sourcetype, updinterval=None, site=None, path=None, login=None, pass=None):
+	''' "source" is a short name of a source
+	sourcetype is one of (ftp, http, https, pop3, scp, file)
+		Not all of these will initially be implemented
+	site is a hostname of where to retrieve files. Blank for "file" sourcetype
+	path is URL components after the hostname, or FTP components, or ...
+		leave blank for pop3
+	login is the login name for the resource (if applicable)
+	pass is the password for the resource (if applicable)
+
+	We may need to figure out a way to work geo and time information in here? Maybe? '''
+	dbconn = psycopg2.connect(PSQL_CONN_STRING)
+	curs = dbconn.cursor()
+	curs.execute("INSERT INTO datasource(sourcename, sourcetype, updinterval, site, path, login, pass) VALUES(%s, %s, %s, %s, %s, %s, %s) RETURNING datsourceid;", (sourcename, sourcetype, updinterval, site, path, login, pass) )
+	return curs.fetchone()[0]
 
 #################################################
 # Source retrieval functions
@@ -64,7 +94,7 @@ def source_do_ihave(source, filename):
 def get_source_info(source):
 # Returns info from define_source
 
-def source_retrieve(source):
+def source_retrieveall(source):
 # Needed for some sourcetypes, like pop3 and *maybe* file?
 #	For these sources, if there is data, it is imported.
 
@@ -72,15 +102,44 @@ def source_retrieve(source):
 # Data retrieval functions
 
 def get_ftplist(site, path, user=None, pass=None):
-
-def get_ftpfile(site, filename, user=None, pass=None):
-	# Seems urllib2 can do this too!
-	# But.... we may want to use a proper FTP module so we can get FTP dirlistings.
 	try:
-		datahandle = urllib2.urlopen(url)
-		return (1, datahandle.read() )
-	except URLError:
-		return (0, None)
+		ftphandle = FTP(site)
+	except Exception:
+		print "Failed to connect to site"
+
+	if(user != None):
+		try:
+			ftphandle.login(user,pass)
+		except Exception:
+			print "Failed to login to site"
+	else:
+		ftphandle.login()
+	files = ftphandle.nlst(path)
+	return files
+
+def get_ftpfile(targfile, site, filename, user=None, pass=None):
+	try:
+		ftphandle = FTP(site)
+	except Exception:
+		print "Failed to connect to site"
+
+	if(user != None):
+		try:
+			ftphandle.login(user,pass)
+		except Exception:
+			print "Failed to login to site"
+	else:
+		ftphandle.login()
+	try:
+		targ = open(targfile, "wb")
+	except Exception:
+		print "Cound not open target filename"
+	try:
+		ftphandle.retrbinary("RETR " + path, targ.write)
+	except Exception:
+		print "Download error"
+	return
+
 
 def get_httpfile(url):
 	try:
