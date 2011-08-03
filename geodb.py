3eb7e296aa4784de62eb1ef8f33a802f3bb00e15
Pat Gunn <pgunn@dachte.org>
2011-08-02 21:57:59 -0400

    Filling in some of these functions

--- geodb.py
+++ geodb.py
@@ -9,34 +9,40 @@ def yearsOfData():
 	"""Returns an integer number of years of data available in the database."""
 	conn = opendb()
 	curs = conn.cursor()
-	curs.execute("")
+	curs.execute("SELECT COUNT(DISTINCT date_part('year', date)) FROM geotimespace;")
+	return curs.fetchone()[0]
 
 def earliestData():
 	"""Returns the earliest date for which data is available. 
 	Not sensitive to source type or data type."""
 	conn = opendb()
 	curs = conn.cursor()
-	curs.execute("")
+	curs.execute("SELECT date_part('year', date) AS geoyear FROM geotimespace ORDER BY geoyear LIMIT 1;")
+	return curs.fetchone()[0]
 
 def latestData():
 	"""Returns the most recent date for which data is available.
 	Not sensitive to source type or data type."""
 	conn = opendb()
 	curs = conn.cursor()
-	curs.execute("")
+	curs.execute("SELECT date_part('year', date) AS geoyear FROM geotimespace ORDER BY geoyear DESC LIMIT 1;")
+	return curs.fetchone()[0]
 
 def earliestSourceData(source,datatype):
 	"""Returns the date of the earliest data entry available for 
 	given source and data type."""
 	conn = opendb()
 	curs = conn.cursor()
-	curs.execute("")
+	curs.execute("SELECT date_part('year', date) AS geoyear FROM geotimespace WHERE locid IN (SELECT locid FROM location WHERE sourceid='%s') ORDER BY geoyear LIMIT 1;", (datatype,))
+	return curs.fetchone()[0]
+
 def latestSourceData(source,datatype):
 	"""Returns the date of the most recent data entry available 
 	for given source and data type."""
 	conn = opendb()
 	curs = conn.cursor()
-	curs.execute("")
+	curs.execute("SELECT date_part('year', date) AS geoyear FROM geotimespace WHERE locid IN (SELECT locid FROM location WHERE sourceid='%s') ORDER BY geoyear DESC LIMIT 1;", (datatype,))
+	return curs.fetchone()[0]
 
 def earliestCompleteData(datatype):
 	"""Returns the earliest date for which all sources have entries 
