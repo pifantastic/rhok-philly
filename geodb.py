22c0e7b2c0183db58d57b57e44952d4e548c6bec
Kate Peterson <inquiryqueue@gmail.com>
2011-08-02 20:54:06 -0400

    Outlining some useful functions related to database contents but not necessary in analysis.py (which is concerned with functions for graphing specific data). I'll be moving some of the data retrieval functions from analysis.py to geodb.py soon.

---
+++ geodb.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+"""  geodb.py contains database functions related to the climate data project, 
+but not involved in the analysis.py process. 
+dbutils.py is for generic database functions not specific to this project."""
+
+from dbutils import *
+
+def yearsOfData():
+    """Returns an integer number of years of data available in the database."""
+    conn = opendb()
+    curs = conn.cursor()
+    curs.execute("")
+
+def earliestData(source,datatype):
+    """Returns the date of the earliest data entry available for 
+    given source and data type."""
+    conn = opendb()
+    curs = conn.cursor()
+    curs.execute("")
+
+def latestData(source,datatype):
+    """Returns the date of the most recent data entry available 
+    for given source and data type."""
+    conn = opendb()
+    curs = conn.cursor()
+    curs.execute("")
+
+def earliestCompleteData(datatype):
+    """Returns the earliest date for which all sources have entries 
+    for given datatype."""
+    conn = opendb()
+    curs = conn.cursor()
+    curs.execute("")
+
+def latestCompleteData(datatype):
+    """Returns the latest date for which all sources have entries 
+    for given datatype."""
+    conn = opendb()
+    curs = conn.cursor()
+    curs.execute("")
+
