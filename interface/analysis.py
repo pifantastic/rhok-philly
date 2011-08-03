b8872248b0ba0a17e2af5c8400c3346e0a9feceb
Pat Gunn <pgunn@dachte.org>
2011-08-02 20:26:34 -0400

    Geodb -> dbutils

--- interface/analysis.py
+++ interface/analysis.py
@@ -6,7 +6,7 @@ Goal: Generate contoured maps of the data plotted by geographical location.
 """
 
 import config, psycopg2, sys, getopt
-from geodb import *
+from dbutils import *
 from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 import numpy as np
